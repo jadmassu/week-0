afd7385cd255fec9c6b4f64d9c68b61d1d6bcf23
eyerusalem <eyerusad12@gmail.com>
2023-12-02 09:25:30 +0300

    typo error added for test

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -4,7 +4,7 @@ def main():
     st.set_page_config(page_title="Slack Report", layout="wide")
     
     col_left, col_right = st.columns((1, 1))
-    col_left.title("Slack Report")
+    col_left.title("Slack Reportt")
     col_left.write("analysis report")
     
 
